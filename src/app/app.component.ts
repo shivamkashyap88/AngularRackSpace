06a888b8468299316a2595eea1d0abe8a493cb65
shivamkashyap88 <shivamkashyap18@gmail.com>
2021-02-01 20:45:18 +0530

    Add CDN for bootstrap

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,22 @@
 import { Component,OnInit } from '@angular/core';
 import {UserData} from './../service/userdata.service';
 import {UserModel} from './../model/user.model';
+import { map } from 'rxjs/operators';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent implements OnInit {
-  public userDataModel: Array<UserModel>;
+  public userDataModel: UserModel[] ;
+  public showData : any[] = [];
   constructor(private userData :UserData) {
     
   }
   ngOnInit(){
-    this.userData.getData().subscribe((resp : UserModel[])=>{
+    this.userData.getData().pipe(map(res=> res)).subscribe((resp : UserModel[])=>{
       this.userDataModel = resp;
+	  console.log('sds',typeof resp); 
     });
   }
 }
